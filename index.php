d007eaee77c0b3e55ba5ac0886bc6b74d60e3310
ImanolCB <imacallebara@gmail.com>
2024-03-06 01:26:47 +0100

    Creación proviional de slider de promociones en el index

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <!-- Bootsrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js" integrity="sha512-+k1pnlgt4F1H8L7t3z95o3/KO+o78INEcXTbnoJQ/F2VqDVhWoaiVml/OEHv9HsVgxUaVW+IbiZPUJQfF/YxZw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
     <!-- CSS -->
     <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="./assets/css/index.css">
@@ -18,12 +20,23 @@
 <body>
     <?php include 'views/includes/header.php' ?>
     <main class="main">
-        <div class="parallax">
+        <section class="parallax">
             <div class="textoTitulo">
-                <p class="h2Titulo">Green Grow</p>
+                <h1>Green Grow</h1>
                 <p class="h2Titulo">Cultiva y dale más vida a tu hogar</p>
             </div>
-        </div>
+
+        </section>
+        <section class="container-fluid">
+            <div class="px-2 ps-md-5 ms-md-1 py-4">
+                <h3>Promociones</h3>
+            </div>
+            <div class="product-list px-2 ps-md-5 ms-md-1">
+                <?php include 'views/includes/slider2.php' ?>
+            </div>
+        </section>
+
+
     </main>
     <?php include 'views/includes/footer.php' ?>
 </body>
---
+++ views/includes/slider.php
@@ -0,0 +1,30 @@
+<div class="me-md-3 py-2">
+    <div class="card border-0 position-relative mx-1">
+        <div class="row">
+            <div class="col-auto">
+                <a href="#">
+                    <img id="imagenPromocion" src="https://images.unsplash.com/photo-1631629571979-1c82e81c7cee?q=80&w=2572&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="promocion">
+                </a>
+            </div>
+            <div class="col my-auto">
+                <div class="card-body px2">
+                    <div class="text-muted">Planta</div>
+                    <a href="#">
+                        <h3 class="card-title">Jazmín</h3>
+                    </a>
+                    <i class="lni lni-star-fill checked"></i>
+                    <i class="lni lni-star-fill checked"></i>
+                    <i class="lni lni-star-fill checked"></i>
+                    <i class="lni lni-star-fill checked"></i>
+                    <i class="lni lni-star-fill checked"></i>
+                    <div class="price-box">
+                        <span class="product-price">100€</span>
+                        <span class="price-before">
+                            <del>20€</del>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
