beb395709a3c5bb9599aaa822733a228b5592957
ImanolCB <imacallebara@gmail.com>
2024-04-03 19:39:46 +0200

    Añadidas las fuentes con google fonts

--- index.php
+++ index.php
@@ -10,6 +10,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
+    <!-- FONT -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
     <!-- CSS -->
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="/assets/css/index.css">
@@ -33,25 +38,25 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-5 ">
-                        <li class="nav-item m-2">
-                            <a class="nav-link active text-nowrap" id="linkNav" aria-current="page" href="/views/shop/promociones.php">Promociones</a>
-                        </li>
-                        <li class="nav-item m-2">
-                            <a class="nav-link active text-nowrap" id="linkNav" aria-current="page" href="/views/shop/tienda.php">Tienda</a>
-                        </li>
-                        <li class="nav-item m-2">
-                            <a class="nav-link active text-nowrap" id="linkNav" aria-current="page" href="#">Libros</a>
-                        </li>
-                        <li class="nav-item m-2">
-                            <a class="nav-link active text-nowrap" id="linkNav" aria-current="page" href="#">Sobre nosotros</a>
-                        </li>
-                    </ul>
-                    <!-- Botones de Iniciar sesión y Registrarse -->
-                    <div class="d-flex">
-                        <a class="btn m-2" id="btnMiCuenta" href="/views/login/login.php">Mi cuenta</a>
-                    </div>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-5 ">
+                    <li class="nav-item m-2">
+                        <a class="nav-link active text-nowrap" id="linkNav" aria-current="page" href="/views/shop/promociones.php">Promociones</a>
+                    </li>
+                    <li class="nav-item m-2">
+                        <a class="nav-link active text-nowrap" id="linkNav" aria-current="page" href="/views/shop/tienda.php">Tienda</a>
+                    </li>
+                    <li class="nav-item m-2">
+                        <a class="nav-link active text-nowrap" id="linkNav" aria-current="page" href="#">Libros</a>
+                    </li>
+                    <li class="nav-item m-2">
+                        <a class="nav-link active text-nowrap" id="linkNav" aria-current="page" href="#">Sobre nosotros</a>
+                    </li>
+                </ul>
+                <!-- Botones de Iniciar sesión y Registrarse -->
+                <div class="d-flex">
+                    <a class="btn m-2" id="btnMiCuenta" href="/views/login/login.php">Mi cuenta</a>
                 </div>
+            </div>
             </div>
         </nav>
     </header>
