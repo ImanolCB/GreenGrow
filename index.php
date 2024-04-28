e8b2e4b4675ababf47e8c3dff0072c4d48179516
ImanolCB <imacallebara@gmail.com>
2024-04-28 21:49:28 +0200

    Modificación de promociones en index, se muestra mediante bucle

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    <?php 
-        require_once './views/includes/fonts.php';
+    <?php
+    require_once './views/includes/fonts.php';
     ?>
 
     <!-- CSS -->
@@ -72,60 +72,21 @@
             <h3>Promociones</h3>
 
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4  row-cols-xxl-5">
-                <div class="col m-10 d-flex justify-content-center">
-                    <?php
-                    echo Producto::crearPromocion(
-                        "https://images.unsplash.com/photo-1589893432683-529de88655a1?q=80&w=2488&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                        "Promocion",
-                        "Descripcion de prueba del producto"
-                    )
-                    ?>
-                </div>
-                <div class="col m-10 d-flex justify-content-center">
-                    <?php
+
+                <?php
+                /**
+                 * Bucle que genera 5 estructuras de promociones
+                 */
+                for ($i = 0; $i < 5; $i++) {
+                    echo '<div class="col m-10 d-flex justify-content-center">';
                     echo Producto::crearPromocion(
                         "https://images.unsplash.com/photo-1485955900006-10f4d324d411?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                         "Promocion",
                         "Descripcion de prueba del producto"
-                    )
-                    ?>
-                </div>
-                <div class="col m-10 d-flex justify-content-center">
-                    <?php
-                    echo Producto::crearPromocion(
-                        "https://images.unsplash.com/photo-1589893432683-529de88655a1?q=80&w=2488&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                        "Promocion",
-                        "Descripcion de prueba del producto"
-                    )
-                    ?>
-                </div>
-                <div class="col m-10 d-flex justify-content-center">
-                    <?php
-                    echo Producto::crearPromocion(
-                        "https://images.unsplash.com/photo-1589893432683-529de88655a1?q=80&w=2488&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                        "Promocion",
-                        "Descripcion de prueba del producto"
-                    )
-                    ?>
-                </div>
-                <div class="col m-10 d-flex justify-content-center">
-                    <?php
-                    echo Producto::crearPromocion(
-                        "https://images.unsplash.com/photo-1485955900006-10f4d324d411?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                        "Promocion",
-                        "Descripcion de prueba del producto"
-                    )
-                    ?>
-                </div>
-                <div class="col m-10 d-flex justify-content-center">
-                    <?php
-                    echo Producto::crearPromocion(
-                        "https://images.unsplash.com/photo-1589893432683-529de88655a1?q=80&w=2488&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                        "Promocion",
-                        "Descripcion de prueba del producto"
-                    )
-                    ?>
-                </div>
+                    );
+                    echo '</div>';
+                }
+                ?>
 
             </div>
 
